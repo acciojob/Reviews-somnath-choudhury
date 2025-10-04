24f666c509c10d519a34f5e029198007d1e1d35c
somnath-choudhury <awesome8somnath@gmail.com>
2025-10-04 17:53:49 +0530

    project completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,18 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react';
+import Review from './Review'
+import "../styles/App.css"; 
+const App = () => {
+  return (
+    <div className='main'>
 
+      <div className='container'>
+        <div>
+          <h1 id='review-heading'>Our Reviews</h1>
+          <Review />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
